ee8deb50421852a5c1f95f1912a1d94be92d742f
Keita Ito <keitaito@keitaito.net>
2026-04-26 22:04:23 +0900

    feat: add Mantine

--- web-client/src/main.tsx
+++ web-client/src/main.tsx
@@ -1,7 +1,9 @@
+import "@mantine/core/styles.css";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { routeTree } from "./routeTree.gen";
+import { MantineProvider } from "@mantine/core";
 
 const router = createRouter({ routeTree });
 
@@ -14,6 +16,8 @@ declare module "@tanstack/react-router" {
 const rootElement = document.getElementById("root") as HTMLElement;
 ReactDOM.createRoot(rootElement).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <MantineProvider>
+      <RouterProvider router={router} />
+    </MantineProvider>
   </StrictMode>,
 );
--- web-client/src/routes/index.tsx
+++ web-client/src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@mantine/core";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
@@ -5,5 +6,9 @@ export const Route = createFileRoute("/")({
 });
 
 function Index() {
-  return <h3>Home</h3>;
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
 }
